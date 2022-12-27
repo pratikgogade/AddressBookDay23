f34f433d4bf8881db2eeb4601a909063073e5aec
unknown <Pratik@Example.com>
2022-12-27 08:57:44 +0530

    [Pratik]Add:Added Program To Create Basic Contact Book

--- AddressBookDay23/Program.cs
+++ AddressBookDay23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program");
+            AddressBook ab = new AddressBook("Pratik", "Gogade", "Ambe Smruti", "Mumbai", "Maharashtra", "psg241999@gmail.com", 410222, 9284664856);
+            ab.DisplayContacts();
+        }
+    }
+}
